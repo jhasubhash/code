caf1bf59b00b979322b891c4b97a3db1d5395faf
Subhash Jha <subhashjha.mail@gmail.com>
2020-07-10 01:03:05 +0530

    Updates

---
+++ precache-manifest.5d292942a8be02fba6b6379f1737a370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be637237a18b8778591928f215c4c340",
+    "url": "/code/index.html"
+  },
+  {
+    "revision": "27ae520c6ef3ee7ef12b",
+    "url": "/code/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "220706daf205e549d9c0",
+    "url": "/code/static/js/2.a24ecc29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/code/static/js/2.a24ecc29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ae520c6ef3ee7ef12b",
+    "url": "/code/static/js/main.39f941a6.chunk.js"
+  },
+  {
+    "revision": "a44dad7e00a722057162",
+    "url": "/code/static/js/runtime-main.1789b56d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/code/static/media/logo.5d5d9eef.svg"
+  }
+]);
