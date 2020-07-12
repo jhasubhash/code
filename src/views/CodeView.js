654c0af52717494516a2410666603a98637947ba
Subhash Jha <subhashjha.mail@gmail.com>
2020-07-12 19:53:49 +0530

    remove first time flicker

--- src/views/CodeView.js
+++ src/views/CodeView.js
@@ -7,6 +7,8 @@ class CodeView extends React.PureComponent {
         super(props);
         this.gistId = "ab55cb8921d8538dabad8d6d2b16afde";
         this.codeMap = [];
+        this.lastId = undefined;
+        this.lastSrc = undefined;
 		this.state = {
             hasFrameLoaded : false
         }
@@ -44,13 +46,18 @@ class CodeView extends React.PureComponent {
 
     getFrameSource = () => {
         let codeId = this.props.pageNum; 
+        if(this.lastId === codeId)
+            return this.lastSrc; 
+        this.lastId = codeId;
         let fontSize = this.props.isMobile?"8px":"14px";
         let wControl = this.props.isMobile?false:true;
         const param1 = "bg=rgba(171%2C%20184%2C%20195%2C%201)&wc="+wControl+"&t=seti&wt=none&l=c&ds=true&dsyoff=10px&dsblur=18px&fs="+fontSize;
         //const param = "bg=rgba(171%2C%20184%2C%20195%2C%201)&t=seti&wt=none&l=c&ds=true&dsyoff=20px&dsblur=68px&wc=true&wa=true&pv=200px&ph=72px&ln=false&fl=1&fm=Hack&fs=14px&lh=133%25&si=false&es=2x&wm=false";
         if(this.codeMap.length <= codeId)
-            return "https://carbon.now.sh/embed/"+this.gistId+"?filename="+codeId+"&"+param1;
-        return "https://carbon.now.sh/embed/[id]?"+param1+"&code="+this.codeMap[codeId];
+            this.lastSrc = "https://carbon.now.sh/embed/"+this.gistId+"?filename="+codeId+"&"+param1;
+        else
+            this.lastSrc = "https://carbon.now.sh/embed/[id]?"+param1+"&code="+this.codeMap[codeId];
+        return this.lastSrc;
     }
 
     iFrameLoaded = (loaded) => {
